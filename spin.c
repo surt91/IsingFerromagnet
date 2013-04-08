8dc6f7758c3ba1996ebd1353dcaf4b9e9778e32a
Hendrik Schawe <surt91@gmail.com>
2013-04-08 20:21:06 +0200

    Monte Carlo angefangen

--- spin.c
+++ spin.c
@@ -9,13 +9,38 @@ int main()
 {
     gs_graph_t *g;
     int L;                                  //!< Kantenlänge des Gitters
-    double sigma = 0.1;
+    double sigma = 0.1; //!< Parameter, der die Verschiebung der einzelnen Knoten bestimmt
+    double E, M;
+    double N;
 
+    srand(100);
+    srand48(100);
+
+    /* Kantenlänge des Feldes */
     L=3;
+    /* Wieviele Sweeps berechnen */
+    /* Ein Sweep sind \f$ L^2 \f$ Monte Carlo Schritte */
+    N=100;
 
+    /* Initialisiere den Graphen für die Spins */
     g = gs_create_graph(L, gauss, sigma);
+
+    /* Verknüpfe die Knoten */
     create_edges_regular(g);
+
+    /* initialisiere den Status der Spins */
+    init_spins_randomly(g);
     print_graph_for_graph_viz(g);
+
+    /* Berechne Energie des Ising Modells */
+    E = calculate_energy(g);
+    fprintf(stderr, "E = %f\n", E);
+    /* Berechne Magnetisierung des Ising Modells */
+    M = calculate_magnetisation(g);
+    fprintf(stderr, "M = %f\n", M);
+
+    /* Führe Monte Carlo Sweeps durch */
+    //~ monte_carlo_sweeps(g, N);
 
     gs_clear_graph(g);
 
@@ -89,4 +114,126 @@ void create_edges_regular(gs_graph_t *g)
         //~ else
             //~ gs_insert_edge(g, i, num_nodes - L + i%L , weight);
     }
+}
+
+/*! \fn void init_spins_randomly(gs_graph_t *g)
+    \brief Weist den Spins im gs_graph_t g zufällige Werte zu
+
+    \param [in,out]    g    Graph, der modifiziert werden soll
+*/
+void init_spins_randomly(gs_graph_t *g)
+{
+    int num_nodes, i;
+
+    num_nodes = g->num_nodes;
+
+    for(i=0;i<num_nodes;i++)
+    {
+        /* Todo: andere rand() Funktion (aus gsl?) */
+        if(drand48()<0.5)
+            g->node[i].spin = 1;
+        else
+            g->node[i].spin = -1;
+    }
+}
+
+/*! \fn void calculate_energy(gs_graph_t *g)
+    \brief Berechnet die Energie des Ising Modells
+
+    Dies wird durch einfache Auswertung des Hamiltonoperators des
+    Ising Modells erledigt. \f[ E = H = -\sum_{<ij>} J_{ij} s_i s_j \f]
+    Dabei ist J die Kopplungskonstante, hier also das Kantengewicht.
+
+    \param [in,out]    g    Graph, der modifiziert werden soll
+    \return Energie des Systems.
+*/
+double calculate_energy(gs_graph_t *g)
+{
+    int num_nodes, i, s1;
+    double E=0;
+    elem_t *list;
+
+    num_nodes = g->num_nodes;
+
+    for(i=0;i<num_nodes;i++)                      /* Über alle Knoten */
+    {
+        s1 = g->node[i].spin;
+        list = g->node[i].neighbors;
+        while(list != NULL)                     /* Über alle Nachbarn */
+        {
+            E += g->node[list->index].spin * list->weight;
+            list = list->next;
+        }
+    }
+    return(E*s1/2);    /* Da über alle Spins zweimal summiert wurde: E/2 */
+}
+
+/*! \fn void calculate_magnetisation(gs_graph_t *g)
+    \brief Berechnet die Magnetisierung des Ising Modells
+
+    Dies wird durch einfache Auswertung der Definition erledigt.
+    \f[ M = \sum_{i} s_i \f]
+
+    \param [in,out]    g    Graph, der modifiziert werden soll
+    \return Magnetisierung des Systems.
+*/
+double calculate_magnetisation(gs_graph_t *g)
+{
+    int num_nodes, i;
+    double M=0;
+
+    num_nodes = g->num_nodes;
+
+    for(i=0;i<num_nodes;i++)                      /* Über alle Knoten */
+        M += g->node[i].spin;
+
+    return(M);
+}
+
+/*! \fn double monte_carlo_sweeps(gs_graph_t *g, int N)
+    \brief Führt N Monte Carlo Sweeps durch
+
+    Dabei besteht ein Sweep aus \f$ L^2 \f$ Monte Carlo Schritten
+
+    \param [in,out]    g    Graph, der modifiziert werden soll
+    \param [in]        N    Anzahl der zu berechnenden Sweeps
+    \return Magnetisierung des Systems.
+*/
+void monte_carlo_sweeps(gs_graph_t *g, int N)
+{
+    int i;
+    int num_nodes;
+    int to_flip_idx;
+    double delta_E;
+    elem_t *list;
+
+    num_nodes = g->num_nodes;
+
+    for(i=0;i<num_nodes*N;i++)
+    {
+        delta_E = 0;
+        /*! Ermittele den zu flippenden Spin zufällig. */
+        to_flip_idx = floor(drand48()*num_nodes);
+        /*! Ermittele Energieänderung
+            \f[ \Delta E = 2s_k \sum_{\mathrm{adj}(s_k)} J_{kj} s_j \f]
+            So wie in \cite newman1999monte S. 52 (3.10) gegeben,
+            Jedoch leicht abgewandelt, um verschiedene J zu
+            berücksichtigen.
+         */
+        list = g->node[to_flip_idx].neighbors;
+        while(list != NULL)
+        {
+            /* Hier wird die Summe ausgeführt: E += s_j * J_{ij} */
+            delta_E += g->node[list->index].spin * list->weight;
+        }
+        /* Hier werden die Koeffizienten berücksichtigt: E = 2s_k*sum */
+        delta_E *= 2 * g->node[to_flip_idx].spin;
+
+        /*! Berechne die Wahrscheinlichkeit, mit der der Flip akzeptiert
+            wird.
+            \f[ A =  \f]*/
+    }
+
+    //~ #ifdef INCREMENTAL
+
 }
--- spin.h
+++ spin.h
@@ -1,4 +1,7 @@
-/*! \file */
+/*! \file spin.h
+ * Kompiliere mit -DINCREMENTAL um bei jedem Monte Carlo Schritt die
+ * Magnetisierung zu updaten
+ */
 #ifndef _SPIN_H
 #define _SPIN_H
 
@@ -11,5 +14,9 @@
 
 double gauss(double sigma);
 void create_edges_regular(gs_graph_t *g);
+void init_spins_randomly(gs_graph_t *g);
+double calculate_energy(gs_graph_t *g);
+double calculate_magnetisation(gs_graph_t *g);
+void monte_carlo_sweeps(gs_graph_t *g, int N);
 
 #endif /* _SPIN_H */
