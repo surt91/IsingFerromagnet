994eda0d7f33637a351a7487640570e919b5b97d
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2013-04-23 15:49:17 +0200

    Datenbanksystem so weit, dass Ergebnisse geplottet werden können

--- auswertung_skripte/db.py
+++ auswertung_skripte/db.py
@@ -6,7 +6,7 @@ import sqlite3
 import zlib
 
 import array
-from numpy import mean
+from numpy import mean, var
 
 from reader import *
 
@@ -15,9 +15,26 @@ class Database():
         self.dbPath = dbPath
 
         if not os.path.isfile(self.dbPath):
+            # Generate new Database in memory
+            self.conn = sqlite3.connect(":memory:")
             self.createNewDatabase(dataPath)
+            self.calculateNewDatabase()
+            # Copy Database to file
+            print "copy Database to {0}".format(self.dbPath)
+            new_db = sqlite3.connect(self.dbPath)
+            query = "".join(line for line in self.conn.iterdump() if "calculated_data" in line)
+            new_db.executescript(query)
+            print "  copied"
+            self.conn.close()
 
+        # lade Datenbank und hole sie in den Speicher
         self.conn = sqlite3.connect(self.dbPath)
+
+        self.writeBinderForGnuplot("Binder")
+        self.writeMeanForGnuplot("Mean")
+        self.writeVarForGnuplot("Var")
+
+        self.conn.close()
 
     @staticmethod
     def getVal(x):
@@ -31,13 +48,80 @@ class Database():
         a.extend(x)
         return buffer(zlib.compress(a.tostring()))
 
+    @staticmethod
+    def getBinder(m):
+        """! Berechnet die Binder Kumulante
+
+            vgl. \cite katzgraber2011introduction S. 12 (19)
+        """
+        m4 = mean([i**4 for i in m])
+        m2 = mean([i**2 for i in m])
+
+        g=(3-m4/(m2**2))/2
+
+        return g
+
+    def writeBinderForGnuplot(self, name):
+        c = self.conn.cursor()
+        for s in self.getSigmas():
+            c.execute('SELECT binder FROM calculated_data WHERE sigma = ? ORDER BY T ASC, L ASC', (s,))
+            x = c.fetchall()
+            x = [i[0] for i in x]
+
+            self.writeFileForGnuplot(name+"_{0}".format(s)+".dat", x)
+
+    def writeMeanForGnuplot(self, name):
+        c = self.conn.cursor()
+        for s in self.getSigmas():
+            c.execute('SELECT meanM FROM calculated_data WHERE sigma = ? ORDER BY T ASC, L ASC', (s,))
+            x = c.fetchall()
+            x = [i[0] for i in x]
+
+            self.writeFileForGnuplot(name+"M_{0}".format(s)+".dat", x)
+        for s in self.getSigmas():
+            c.execute('SELECT meanE FROM calculated_data WHERE sigma = ? ORDER BY T ASC, L ASC', (s,))
+            x = c.fetchall()
+            x = [i[0] for i in x]
+
+            self.writeFileForGnuplot(name+"E_{0}".format(s)+".dat", x)
+    def writeVarForGnuplot(self, name):
+        c = self.conn.cursor()
+        for s in self.getSigmas():
+            c.execute('SELECT varM FROM calculated_data WHERE sigma = ? ORDER BY T ASC, L ASC', (s,))
+            x = c.fetchall()
+            x = [i[0] for i in x]
+
+            self.writeFileForGnuplot(name+"M_{0}".format(s)+".dat", x)
+        for s in self.getSigmas():
+            c.execute('SELECT varE FROM calculated_data WHERE sigma = ? ORDER BY T ASC, L ASC', (s,))
+            x = c.fetchall()
+            x = [i[0] for i in x]
+
+            self.writeFileForGnuplot(name+"E_{0}".format(s)+".dat", x)
+
+    def writeFileForGnuplot(self, name, x):
+        Ls = sorted(list(set(self.getLs())))
+        Ts = sorted(list(set(self.getTs())))
+        numL = len(set(Ls))
+        numT = len(set(Ts))
+
+        f = open(name, "w")
+        f.write("# L: ")
+        for l in Ls:
+            f.write(" {0}".format(l))
+
+        # Zeilen
+        for i in range(numT):
+            f.write("\n{0}".format(Ts[i]))
+            # Spalten
+            for j in range(numL):
+                f.write(" {0}".format(x[j+i*numL]))
+
     def createNewDatabase(self, dataPath):
         print("create new Database: '{0}'".format(self.dbPath))
 
-        conn = sqlite3.connect(self.dbPath)
-        conn.execute("""CREATE TABLE rawdata
+        self.conn.execute("""CREATE TABLE rawdata
             (n blob, sigma real, L integer, x integer, T real, M blob, E blob)""")
-
 
         for f in os.listdir(dataPath):
             if not ".dat" in f:
@@ -45,39 +129,64 @@ class Database():
 
             r = output_reader(os.path.join(dataPath,f))
             t = [(self.setVal(r.N), r.sigma, r.L, r.x, r.T[i], self.setVal(r.M[i]), self.setVal(r.E[i])) for i in range(len(r.T))]
-            conn.executemany('INSERT INTO rawdata VALUES (?,?,?,?,?,?,?)', t)
-        conn.commit()
-        conn. close()
+            self.conn.executemany('INSERT INTO rawdata VALUES (?,?,?,?,?,?,?)', t)
+            self.conn.commit()
 
-    def addRows(self, dataLst):
-        """dataLst ist eine liste von Datentupeln. bsp:
-         [(r.N, r.sigma, r.L, r.x, r.T[i], r.M[i], r.E[i]),
-          (r.N, r.sigma, r.L, r.x, r.T[i], r.M[i], r.E[i])]
-        """
-        self.conn.executemany('INSERT INTO rawdata VALUES (?,?,?,?,?,?,?)', dataLst)
+    def calculateNewDatabase(self):
+        self.conn.execute("""CREATE TABLE calculated_data
+            (sigma real, L integer, T real, binder real, meanM real, meanE real, varM real, varE real)""")
+
+        sigmas = self.getSigmas()
+        Ls = self.getLs()
+        Ts = self.getTs()
+        rows=[]
+        for s in sigmas:
+            print "sigma: ", s
+            for L in Ls:
+                print "L: ", L
+                for T in Ts:
+                    binder = self.getAverageM(self.getBinder, s, L, T)
+                    mM = self.getAverageM(mean, s, L, T)
+                    mE = self.getAverageE(mean, s, L, T)
+                    vM = self.getAverageM(var, s, L, T)
+                    vE = self.getAverageE(var, s, L, T)
+                    rows.append((s, L, T, binder, mM, mE, vM, vE))
+        self.conn.executemany('INSERT INTO calculated_data VALUES (?,?,?,?,?,?,?,?)', rows)
         self.conn.commit()
 
     def getXs(self):
         c = self.conn.cursor()
-        c.execute('SELECT x FROM rawdata')
+        try:
+            c.execute('SELECT x FROM rawdata')
+        except :
+            c.execute('SELECT x FROM calculated_data')
         x = c.fetchall()
         x = (i[0] for i in x)
         return list(set(x))
     def getTs(self):
         c = self.conn.cursor()
-        c.execute('SELECT T FROM rawdata')
+        try:
+            c.execute('SELECT T FROM rawdata')
+        except :
+            c.execute('SELECT T FROM calculated_data')
         x = c.fetchall()
         x = (i[0] for i in x)
         return list(set(x))
     def getLs(self):
         c = self.conn.cursor()
-        c.execute('SELECT L FROM rawdata')
+        try:
+            c.execute('SELECT L FROM rawdata')
+        except :
+            c.execute('SELECT L FROM calculated_data')
         x = c.fetchall()
         x = (i[0] for i in x)
         return list(set(x))
     def getSigmas(self):
         c = self.conn.cursor()
-        c.execute('SELECT sigma FROM rawdata')
+        try:
+            c.execute('SELECT sigma FROM rawdata')
+        except :
+            c.execute('SELECT sigma FROM calculated_data')
         x = c.fetchall()
         x = (i[0] for i in x)
         return list(set(x))
@@ -85,13 +194,19 @@ class Database():
     def getMForSigmaLT(self, sigma, L, T, x):
         c = self.conn.cursor()
         c.execute('SELECT M FROM rawdata WHERE sigma = ? AND L = ? AND T = ? AND x = ?', (sigma, L, T, x))
-        M = self.getVal(c.fetchone()[0])
+        try:
+            M = self.getVal(c.fetchone()[0])
+        except TypeError:
+            M = [0]
         c.close()
         return M
     def getEForSigmaLT(self, sigma, L, T, x):
         c = self.conn.cursor()
         c.execute('SELECT E FROM rawdata WHERE sigma = ? AND L = ? AND T = ? AND x = ?', (sigma, L, T, x))
-        E = self.getVal(c.fetchone()[0])
+        try:
+            E = self.getVal(c.fetchone()[0])
+        except TypeError:
+            E = [0]
         c.close()
         return E
 
@@ -104,3 +219,5 @@ class Database():
         return mean([self.getExpectationM(f, sigma, L, T, x) for x in self.getXs()])
     def getAverageE(self, f, sigma, L, T):
         return mean([self.getExpectationE(f, sigma, L, T, x) for x in self.getXs()])
+
+a=Database()
