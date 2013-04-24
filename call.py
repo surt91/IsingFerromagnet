d7b13db0f8bc9f1373f7c450b40f0e9258048300
Hendrik Schawe <surt91@gmail.com>
2013-04-24 13:52:07 +0200

    Kleinigkeiten, Parameter angepasst/Aufräumaktion

--- call.py
+++ call.py
@@ -4,7 +4,7 @@ from subprocess import call
 from numpy import arange
 
 def f(config):
-    t=",".join([str(i) for i in arange(1.0,3.3,0.1)]) #passend fur 16 und 32 fur grossere muss ich noch finden
+    t=",".join([str(i) for i in arange(0.1,3.3,0.1)]) #passend fur 16 und 32 fur grossere muss ich noch finden
     for s in arange(0,1.3,0.1):
         for [l,t_eq] in config:
             if s == 0: end = 1
@@ -14,15 +14,15 @@ def f(config):
                        "-L{0}".format(l), "-e{0}".format(t_eq),
                        "-N{0}".format(t_eq+500),
                        "-v", "-u0", "-x{0}".format(x), "-w", "-p",
-                       "-s{0}".format(s), "-a0.5", "-gOut_s_{0}_x_{1}_L{2}.svg".format(s,x,l),
+                       "-s{0}".format(s), "-a0.5", #"-gOut_s_{0}_x_{1}_L{2}.svg".format(s,x,l),
                        "-t1"])
 
 if __name__ == '__main__':
     # Tupel aus L, T_eq
     # Also Grosse und Equilibriums Zeit
     # Diese sind durch tests in zeiten.par eingetragen
-    configs = [ ( 16,  200*2),
-                ( 32,  400*2)
+    configs = [ ( 16,  30*2),
+                ( 32,  50*2)
                 #~ ( 64, 1000*2)
                 #~ (128, 2000*2)
                                   ]
