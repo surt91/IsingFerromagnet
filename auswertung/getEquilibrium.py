de57508868c17845b07c84c518c44de5b4237783
Hendrik Schawe <surt91@gmail.com>
2013-04-30 20:34:05 +0200

    Korrektur: es werden nun alle Temperaturen benutzt

--- auswertung/getEquilibrium.py
+++ auswertung/getEquilibrium.py
@@ -13,7 +13,7 @@ if not os.path.exists(directory):
 outDirectory = "../data/equi_plots"
 if not os.path.exists(outDirectory):
     os.makedirs(outDirectory)
-N = 150
+N = 300
 
 config = getConfig()
 
@@ -39,13 +39,14 @@ for filename in os.listdir(directory):
     if ".dat" in filename and "up" in filename :
         i+=1
         filenameWithPath=os.path.join(directory,filename)
+        r = output_reader(filenameWithPath)
         f = open("plot_tmp.gp", "w")
-        for j in range(3,66,2):
+        for j in range(3,len(r.T)*2+3,2):
             f.write("set terminal png\n")
-            f.write("set output '{0}'\n".format(os.path.join(outDirectory,filename)+"{0}_T_{1}_M".format(i,t.split(",")[j/2-1])+".png"))
+            f.write("set output '{0}'\n".format(os.path.join(outDirectory,filename)+"{0}_T_{1}_M".format(i,r.T[j/2-1])+".png"))
             f.write("plot '{0}' using 1:(abs(${1})) w l, '{2}' using 1:(abs(${1})) w l\n\n".format(filenameWithPath,j,filenameWithPath.replace("up","ra")))
             f.write("set terminal png\n")
-            f.write("set output '{0}'\n".format(os.path.join(outDirectory,filename)+"{0}_T_{1}_E".format(i,t.split(",")[j/2-1])+".png"))
+            f.write("set output '{0}'\n".format(os.path.join(outDirectory,filename)+"{0}_T_{1}_E".format(i,r.T[j/2-1])+".png"))
             f.write("plot '{0}' using 1:{1} w l, '{2}' using 1:{1} w l\n\n\n".format(filenameWithPath,j-1,filenameWithPath.replace("up","ra")))
         f.close()
 
