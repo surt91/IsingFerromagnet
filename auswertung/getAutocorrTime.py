9fa5edc9b0f50c5671caf2e339e1f363a992fe00
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2013-04-30 15:58:46 +0200

    Jetzt Plots statt Tabellen

--- auswertung/getAutocorrTime.py
+++ auswertung/getAutocorrTime.py
@@ -39,9 +39,11 @@ if __name__ == "__main__":
     N = 500
 
     config = getConfig()
+    sigmas = [0,1]
 
-    for s in [0,1]:
-        for [l,t,t_eq,tau] in config:
+    out = [[0 for y in sigmas] for x in config]
+    for [i,s] in enumerate(sigmas):
+        for [j,[l,t,t_eq,tau]] in enumerate(config):
             filename = "{2}/S_{0}_L_{1}_ra.dat".format(s,l, directory)
             call(["time", "../test", "-T{0}".format(t),
                    "-L{0}".format(l), "-e{0}".format(t_eq),
@@ -50,10 +52,33 @@ if __name__ == "__main__":
                    "-s{0}".format(s), "-a0.5",
                    "-t1", "-o{0}".format(filename)])
             r = output_reader(filename)
-            tauM = [getAutocorrTime(r.M[i]) for i in range(len(r.T))]
-            print "L = {0}".format(l)
-            print "T tau"
-            for i in range(len(r.T)):
-                print "{0}  {1}".format(r.T[i], tauM[i])
-            print
-            print
+            tauM = [getAutocorrTime(r.M[n]) for n in range(len(r.T))]
+            out[j][i] = (r.T,tauM)
+
+    for [j,[l,t,t_eq,tau]] in enumerate(config):
+        name="S_{0}_L_{1}_ra".format(s,l)
+        filename = os.path.join(directory,name+".dat")
+        f = open(filename+".gp", "w")
+        #~ f.write("set terminal  postscript eps enhanced\n")
+        #~ f.write("set output '{0}'\n".format(name+".eps"))
+        f.write("set terminal png\n")
+        f.write("set output '{0}'\n".format(name+".png"))
+        f.write("set xlabel 'Temperatur'\n")
+        f.write("set ylabel '{0}'\n".format("Autokorrelationszeit"))
+        f.write("set key right\n")
+
+        f.write("plot ")
+        tmpStr = ""
+        for [i,s] in enumerate(sigmas):
+            tmpStr+=("'{0}' using 1:{1} w l title {2}, ".format(name+".dat", i+2, "'sigma" +" = {0}'".format(s)))
+        f.write(tmpStr[:-2])
+        f.close()
+
+        f = open(filename, "w")
+        for [n,T] in enumerate(out[j][i][0]):
+            f.write("{0}".format(T))
+            for [i,s] in enumerate(sigmas):
+                # Spalten
+                f.write(" {0}  ".format(out[j][i][1][n]))
+            f.write("\n")
+        f.close()
