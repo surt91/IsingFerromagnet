f073bd177c1aab0f228d99b7bffa2633ed6bd851
Hendrik Schawe <surt91@gmail.com>
2013-06-06 17:44:49 +0200

    neue Parameter fur GG Graphen

--- auswertung/getConfig.py
+++ auswertung/getConfig.py
@@ -10,7 +10,8 @@ def getConfig():
 
     sigmas = (0.00, 0.03, 0.05, 0.08, 0.10, 0.12, 0.15, 0.20, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2)
 
-    # minimal:
+    ####################################################################
+    # RNG:
     #~ t16=",".join([str(i) for i in arange(0.1,2.0,0.15)]+[str(i) for i in arange(2.0,3.3,0.2)])
     # genug Daten an Tc:
     t16l=",".join([str(i) for i in arange(0.1,1.8,0.15)]+[str(i) for i in arange(1.8,2.4,0.02)]+[str(i) for i in arange(2.4,3.3,0.2)])
@@ -23,6 +24,32 @@ def getConfig():
     t32l=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,1.8,0.09)]+[str(i) for i in arange(1.8,2.4,0.02)]+[str(i) for i in arange(2.4,3.3,0.09)])
     t32m=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,1.4,0.09)]+[str(i) for i in arange(1.4,2.0,0.02)]+[str(i) for i in arange(2.0,3.3,0.09)])
     t32h=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,1.1,0.09)]+[str(i) for i in arange(1.1,1.5,0.02)]+[str(i) for i in arange(1.5,3.3,0.09)])
+    # minimal:
+    #~ t64l=",".join([str(i) for i in arange(0.1,0.9,0.15)]+[str(i) for i in arange(0.9,2.30,0.03)]+[str(i) for i in arange(2.30,3.3,0.05)]) #passend fur 64
+    #~ t64h=",".join([str(i) for i in arange(0.1,0.8,0.08)]+[str(i) for i in arange(0.8,1.50,0.02)]+[str(i) for i in arange(1.5,2.30,0.035)]+[str(i) for i in arange(2.30,3.3,0.07)]) #passend fur 64
+    t64l=",".join([str(i) for i in arange(0.1,0.9,0.15)]+[str(i) for i in arange(0.9,2.30,0.03)]+[str(i) for i in arange(2.30,3.3,0.05)]) #passend fur 64
+    t64m=",".join([str(i) for i in arange(0.1,0.9,0.15)]+[str(i) for i in arange(0.9,2.30,0.03)]+[str(i) for i in arange(2.30,3.3,0.05)]) #passend fur 64
+    t64h=",".join([str(i) for i in arange(0.1,0.8,0.08)]+[str(i) for i in arange(0.8,1.50,0.02)]+[str(i) for i in arange(1.5,2.30,0.035)]+[str(i) for i in arange(2.30,3.3,0.07)]) #passend fur 64
+    t128l=",".join([str(i) for i in arange(0.1,0.8,0.1)]+[str(i) for i in arange(0.8,1.42,0.03)]+[str(i) for i in arange(1.42,2.08,0.02)]+[str(i) for i in arange(2.08,2.44,0.01)]+[str(i) for i in arange(2.44,3.3,0.03)]) #passend fur 128
+    t128m=",".join([str(i) for i in arange(0.1,0.82,0.05)]+[str(i) for i in arange(0.82,1.42,0.01)]+[str(i) for i in arange(1.42,2.08,0.015)]+[str(i) for i in arange(2.08,2.44,0.02)]+[str(i) for i in arange(2.44,3.3,0.03)]) #passend fur 128
+    t128h=",".join([str(i) for i in arange(0.1,0.82,0.05)]+[str(i) for i in arange(0.82,1.42,0.01)]+[str(i) for i in arange(1.42,2.08,0.015)]+[str(i) for i in arange(2.08,2.44,0.03)]+[str(i) for i in arange(2.44,3.3,0.035)]) #passend fur 128
+    t256l=",".join([str(i) for i in arange(0.1,0.55,0.1)]+[str(i) for i in arange(0.55,1.5,0.06)]+[str(i) for i in arange(1.5,1.8,0.008)]+[str(i) for i in arange(1.8,2.08,0.01)]+[str(i) for i in arange(2.08,2.56,0.007)]+[str(i) for i in arange(2.56,3.3,0.017)]) #passend fur 256
+    t256m=",".join([str(i) for i in arange(0.1,0.55,0.1)]+[str(i) for i in arange(0.55,1.5,0.006)]+[str(i) for i in arange(1.5,1.8,0.004)]+[str(i) for i in arange(1.8,2.08,0.01)]+[str(i) for i in arange(2.08,2.44,0.007)]+[str(i) for i in arange(2.44,3.3,0.017)]) #passend fur 256
+    t256h=",".join([str(i) for i in arange(0.1,0.55,0.06)]+[str(i) for i in arange(0.55,1.0,0.009)]+[str(i) for i in arange(1.0,1.5,0.006)]+[str(i) for i in arange(1.5,1.8,0.01)]+[str(i) for i in arange(1.8,2.08,0.010)]+[str(i) for i in arange(2.08,2.44,0.01)]+[str(i) for i in arange(2.44,3.3,0.02)]) #passend fur 256
+    ####################################################################
+    # GG:
+    #~ t16=",".join([str(i) for i in arange(0.1,2.0,0.15)]+[str(i) for i in arange(2.0,3.3,0.2)])
+    # genug Daten an Tc:
+    t16l=",".join([str(i) for i in arange(0.1,2.0,0.15)]+[str(i) for i in arange(2.0,2.4,0.2)]+[str(i) for i in arange(2.4,3.0,0.02)+[str(i) for i in arange(3.0,3.3,0.2)])
+    t16m=",".join([str(i) for i in arange(0.1,2.0,0.15)]+[str(i) for i in arange(2.0,2.4,0.2)]+[str(i) for i in arange(2.4,3.0,0.02)+[str(i) for i in arange(3.0,3.3,0.2)])
+    t16h=",".join([str(i) for i in arange(0.1,2.0,0.15)]+[str(i) for i in arange(2.0,2.5,0.02)]+[str(i) for i in arange(2.5,3.3,0.2)])
+    # minimal:
+    #~ t32l=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,3.3,0.09)])
+    #~ t32h=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,3.3,0.09)])
+    # genug Daten an Tc:
+    t32l=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,2.4,0.09)]+[str(i) for i in arange(2.4,3.0,0.02)+[str(i) for i in arange(3.0,3.3,0.2)])
+    t32m=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,2.4,0.09)]+[str(i) for i in arange(2.4,3.0,0.02)+[str(i) for i in arange(3.0,3.3,0.2)])
+    t32h=",".join([str(i) for i in arange(0.1,0.9,0.14)]+[str(i) for i in arange(0.9,2.0,0.09)]+[str(i) for i in arange(2.0,2.5,0.02)]+[str(i) for i in arange(2.5,3.3,0.2)])
     # minimal:
     #~ t64l=",".join([str(i) for i in arange(0.1,0.9,0.15)]+[str(i) for i in arange(0.9,2.30,0.03)]+[str(i) for i in arange(2.30,3.3,0.05)]) #passend fur 64
     #~ t64h=",".join([str(i) for i in arange(0.1,0.8,0.08)]+[str(i) for i in arange(0.8,1.50,0.02)]+[str(i) for i in arange(1.5,2.30,0.035)]+[str(i) for i in arange(2.30,3.3,0.07)]) #passend fur 64
@@ -59,10 +86,10 @@ def getConfig():
 
     #configs = c16 + c32 + c64 + c128 + c256
     #configs = c128 + c64 + c32 + c16
-    #configs = c16
+    configs = c16 + c32
     #configs = c32
     #configs = c64
-    configs = c128
+    #configs = c128
     #configs = c256
     #configs = c256 + c128 + c64
 
