9d63a47886adfc4bc0aa66e264bc5797c2a6ba61
Hendrik Schawe <hendrik.schawe@uni-oldenburg.de>
2013-06-14 15:38:16 +0200

    binder fit: etwas eleganter und sucht auch den y Schnittpunkt

--- auswertung/fitPolyAndFindIntersection.py
+++ auswertung/fitPolyAndFindIntersection.py
@@ -20,9 +20,9 @@ logging.basicConfig(level=logging.INFO,
 
 class BinderIntersections():
 
-    def __init__(self, database="data.db", deg=4 ):
+    def __init__(self, directory, database="data.db", deg=4 ):
         self.deg = deg
-
+        self.directory = directory
         self.conn = sqlite3.connect(database)
 
     def getBinderForSigmaAndL(self, sigma, L):
@@ -60,32 +60,33 @@ class BinderIntersections():
         if write:
             self.writeForGnuplot(p1, p2, x1, x2, y1, y2, dy1, dy2, L1, L2, sigma)
 
-        result = self.findIntersection(lambda x : numpy.polyval(p1,x), lambda x : numpy.polyval(p2,x), (self.up + self.down)/2)
-        return result
+        x = self.findIntersection(lambda z : numpy.polyval(p1,z), lambda z : numpy.polyval(p2,z), (self.up + self.down)/2)
+        y = numpy.polyval(p1,x)
+        return x,y
 
     def writeForGnuplot(self, p1, p2, x1, x2, y1, y2, dy1, dy2, L1, L2, sigma):
         X = numpy.arange(self.down, self.up, (self.up - self.down)/500)
-        with open(os.path.join(directory,"tmpFit{0}_s_{1:.3f}.dat".format(L1,sigma)), "w") as f:
+        with open(os.path.join(self.directory,"tmpFit{0}_s_{1:.3f}.dat".format(L1,sigma)), "w") as f:
             for x in X:
                 f.write(" ".join([str(x), str(numpy.polyval(p1,x))]))
                 f.write("\n")
 
-        with open(os.path.join(directory,"tmpFit{0}_s_{1:.3f}.dat".format(L2,sigma)), "w") as f:
+        with open(os.path.join(self.directory,"tmpFit{0}_s_{1:.3f}.dat".format(L2,sigma)), "w") as f:
             for x in X:
                 f.write(" ".join([str(x), str(numpy.polyval(p2,x))]))
                 f.write("\n")
 
-        with open(os.path.join(directory,"tmp{0}_s_{1:.3f}.dat".format(L1,sigma)), "w") as f:
+        with open(os.path.join(self.directory,"tmp{0}_s_{1:.3f}.dat".format(L1,sigma)), "w") as f:
             for [x,y,dy] in zip(map(str,x1), map(str,y1), map(str,dy1)):
                 f.write(" ".join([x, y, dy]))
                 f.write("\n")
 
-        with open(os.path.join(directory,"tmp{0}_s_{1:.3f}.dat".format(L2,sigma)), "w") as f:
+        with open(os.path.join(self.directory,"tmp{0}_s_{1:.3f}.dat".format(L2,sigma)), "w") as f:
             for [x,y,dy] in zip(map(str,x2), map(str,y2), map(str,dy2)):
                 f.write(" ".join([x, y, dy]))
                 f.write("\n")
 
-        with open(os.path.join(directory,"plotAll_s_{0:.3f}.gp".format(sigma)), "w") as f:
+        with open(os.path.join(self.directory,"plotAll_s_{0:.3f}.gp".format(sigma)), "w") as f:
             f.write("set terminal png\n")
             f.write('set output "out_s_{0:.3f}.png"\n'.format(sigma))
             f.write('plot "tmpFit16_s_{0:.3f}.dat" u 1:2 w l lc rgb "red" title "Fit: L = 16", \
@@ -102,55 +103,58 @@ class BinderIntersections():
 
 if __name__ == "__main__":
 
-    sizes = [16,32,64,128]
+    def writeAndPlot(graphType):
+        sizes = [16,32,64,128]
 
-    logging.info("possible intersections: " + str([x for x in combinations(sizes, 2)]))
+        logging.info("possible intersections: " + str([x for x in combinations(sizes, 2)]))
+        sigmas    = (0.00, 0.03, 0.05, 0.08, 0.10, 0.12, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20)
 
-    sigmas    = (0.00, 0.03, 0.05, 0.08, 0.10, 0.12, 0.15, 0.20, 0.25, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00, 1.10, 1.20)
-    downsRNG  = (2.25, 2.25, 2.25, 2.23, 2.18, 2.07, 1.85, 1.60, 1.45, 1.38, 1.30, 1.24, 1.22, 1.20, 1.18, 1.17, 1.18, 1.18, 1.18)
-    upsRNG    = (2.30, 2.30, 2.30, 2.30, 2.25, 2.15, 1.95, 1.68, 1.53, 1.45, 1.38, 1.32, 1.30, 1.28, 1.26, 1.26, 1.24, 1.24, 1.24)
-    downsGG   = (2.25, 2.80, 2.80, 2.80, 2.85, 2.85, 2.85, 2.80, 2.60, 2.48, 2.25, 2.20, 2.15, 2.12, 2.10, 2.08, 2.08, 2.08, 2.08)
-    upsGG     = (2.30, 2.94, 2.95, 2.95, 2.95, 3.00, 3.00, 2.90, 2.75, 2.60, 2.40, 2.30, 2.25, 2.22, 2.22, 2.20, 2.20, 2.20, 2.20)
-    results = {}
+        x = {}
+        y = {}
 
+        if graphType == "GG":
+            directory = "../dataGG/binderIntersect/"
+            db = "dataGG.db"
+            downs   = (2.25, 2.80, 2.80, 2.80, 2.85, 2.85, 2.85, 2.80, 2.60, 2.48, 2.25, 2.20, 2.15, 2.12, 2.10, 2.08, 2.08, 2.08, 2.08)
+            ups     = (2.30, 2.94, 2.95, 2.95, 2.95, 3.00, 3.00, 2.90, 2.75, 2.60, 2.40, 2.30, 2.25, 2.22, 2.22, 2.20, 2.20, 2.20, 2.20)
 
-    directory = "../dataRNG/binderIntersect/"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    a = BinderIntersections("dataRNG.db")
-    for [s, down, up] in zip(sigmas, downsRNG, upsRNG):
-        tmpIntersects = [a.getIntersection(s, L1, L2, down=down, up=up) for [L1,L2] in combinations(sizes, 2)]
-        results.update({s:tmpIntersects})
-
-    with open(os.path.join(directory,"Tc.dat"), "w") as f:
-        for s in sigmas:
-            f.write("{0} {1} {2}\n".format(s, mean(results[s]), std(results[s])))
-
-    with open(os.path.join(directory,"plotTc.gp"), "w") as f:
-        f.write("set terminal png\n")
-        f.write('set output "Tc.png"\n')
-        f.write("plot 'Tc.dat' w ye\n")
-        f.write("set xlabel 'sigma'\n")
-        f.write("set ylabel 'T'\n")
-        f.write("set key right\n")
+        else:
+            directory = "../dataRNG/binderIntersect/"
+            db = "dataRNG.db"
+            downs  = (2.25, 2.25, 2.25, 2.23, 2.18, 2.07, 1.85, 1.60, 1.45, 1.38, 1.30, 1.24, 1.22, 1.20, 1.18, 1.17, 1.18, 1.18, 1.18)
+            ups    = (2.30, 2.30, 2.30, 2.30, 2.25, 2.15, 1.95, 1.68, 1.53, 1.45, 1.38, 1.32, 1.30, 1.28, 1.26, 1.26, 1.24, 1.24, 1.24)
 
 
-    directory = "../dataGG/binderIntersect/"
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    a = BinderIntersections("dataGG.db")
-    for [s, down, up] in zip(sigmas, downsGG, upsGG):
-        tmpIntersects = [a.getIntersection(s, L1, L2, down=down, up=up) for [L1,L2] in combinations(sizes, 2)]
-        results.update({s:tmpIntersects})
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        a = BinderIntersections(directory, db)
+        for [s, down, up] in zip(sigmas, downs, ups):
+            tmpIntersects = [a.getIntersection(s, L1, L2, down=down, up=up) for [L1,L2] in combinations(sizes, 2)]
+            x.update({s:[i[0] for i in tmpIntersects]})
+            y.update({s:[i[1] for i in tmpIntersects]})
 
-    with open(os.path.join(directory,"Tc.dat"), "w") as f:
-        for s in sigmas:
-            f.write("{0} {1} {2}\n".format(s, mean(results[s]), std(results[s])))
+        with open(os.path.join(directory,"Tc.dat"), "w") as f:
+            for s in sigmas:
+                f.write("{0} {1} {2}\n".format(s, mean(x[s]), std(x[s])))
+        with open(os.path.join(directory,"TcG.dat"), "w") as f:
+            for s in sigmas:
+                f.write("{0} {1} {2}\n".format(s, mean(y[s]), std(y[s])))
 
-    with open(os.path.join(directory,"plotTc.gp"), "w") as f:
-        f.write("set terminal png\n")
-        f.write('set output "Tc.png"\n')
-        f.write("plot 'Tc.dat' w ye\n")
-        f.write("set xlabel 'sigma'\n")
-        f.write("set ylabel 'T'\n")
-        f.write("set key right\n")
+        with open(os.path.join(directory,"plotTc.gp"), "w") as f:
+            f.write("set terminal png\n")
+            f.write('set output "Tc.png"\n')
+            f.write("plot 'Tc.dat' u 1:2:3 w ye \n")
+            f.write("set xlabel 'sigma'\n")
+            f.write("set ylabel 'T'\n")
+            f.write("set key right\n")
+
+        with open(os.path.join(directory,"plotTcG.gp"), "w") as f:
+            f.write("set terminal png\n")
+            f.write('set output "TcG.png"\n')
+            f.write("plot 'TcG.dat' u 1:2:3 w ye\n")
+            f.write("set xlabel 'sigma'\n")
+            f.write("set ylabel 'g(T)'\n")
+            f.write("set key right\n")
+
+    writeAndPlot("GG")
+    writeAndPlot("RNG")
