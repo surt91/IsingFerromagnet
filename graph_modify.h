2fa15ec5194093c9207a157af7aeb6101caf1008
Hendrik Schawe <surt91@gmail.com>
2021-02-10 13:08:00 +0100

    removed wrong `inline` declarations
    how did this ever compile?

--- graph_modify.h
+++ graph_modify.h
@@ -28,10 +28,10 @@ void move_graph_nodes(gs_graph_t *g, double (*f)(const gsl_rng *, double), gsl_r
 
 void create_edges(gs_graph_t *g, const options_t o);
 void create_edges_mst(gs_graph_t *g, const options_t o);
-inline int check_relative_neighborhood(double dist12, gs_node_t node1, gs_node_t node2, gs_node_t node3);
-inline void get_cell_border_relative_neighborhood(gs_node_t node1, gs_node_t node2, double dist12, int L, int *x0, int *x1, int *y0, int *y1);
-inline int check_gabriel(double dist12, gs_node_t node1, gs_node_t node2, gs_node_t node3);
-inline void get_cell_border_gabriel(gs_node_t node1, gs_node_t node2, double dist12, int L, int *x0, int *x1, int *y0, int *y1);
+int check_relative_neighborhood(double dist12, gs_node_t node1, gs_node_t node2, gs_node_t node3);
+void get_cell_border_relative_neighborhood(gs_node_t node1, gs_node_t node2, double dist12, int L, int *x0, int *x1, int *y0, int *y1);
+int check_gabriel(double dist12, gs_node_t node1, gs_node_t node2, gs_node_t node3);
+void get_cell_border_gabriel(gs_node_t node1, gs_node_t node2, double dist12, int L, int *x0, int *x1, int *y0, int *y1);
 void delete_random_edges_till_percolation(gs_graph_t *g, gsl_rng *rng);
 int test_connectivity_between_nodes(gs_graph_t *g, int node1, int node2);
 node_t * gs_depth_first_search(gs_graph_t *g, int node, int target, node_t *tree_of_tested_nodes, int *status);
